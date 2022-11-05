ccb70b3c6d5057d24a34e0f3431750dc2b313f52
anton.prihodko <anton.prihodko@acronis.com>
2022-11-05 12:46:35 +0400

    Added Search for Category

---
+++ src/main/java/com/foxminded/aprihodko/carrestservice/repository/dao/specification/Specification.java
@@ -0,0 +1,11 @@
+package com.foxminded.aprihodko.carrestservice.repository.dao.specification;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+public interface Specification<T> {
+
+	Predicate toPredicate(Root<T> root, CriteriaQuery query, CriteriaBuilder cb);
+}
