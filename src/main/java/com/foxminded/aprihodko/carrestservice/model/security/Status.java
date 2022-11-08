618f278d7668c3a050faa23d3fe09bcc79fbd7de
anton.prihodko <anton.prihodko@acronis.com>
2022-11-08 16:31:42 +0400

    Deleted files

--- src/main/java/com/foxminded/aprihodko/carrestservice/model/security/Status.java
+++
@@ -1,5 +0,0 @@
-package com.foxminded.aprihodko.carrestservice.model.security;
-
-public enum Status {
-	ACTIVE, NOT_ACTIVE, DELETED
-}
--- src/test/resources/sql/make_test_data.sql
+++
@@ -1,3 +0,0 @@
-insert into makes (id, make_name)
-VALUES (100, 'Audi'),
-       (101, 'BMW');
