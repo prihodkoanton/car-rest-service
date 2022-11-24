bfa054bc69ccd025af4abbd20e1a975d6bd1db29
anton.prihodko <anton.prihodko@acronis.com>
2022-11-24 11:18:29 +0400

    Added Search method. And empty test for Controller

--- src/main/java/com/foxminded/aprihodko/carrestservice/controller/CarRestController.java
+++ src/main/java/com/foxminded/aprihodko/carrestservice/controller/CarRestController.java
@@ -2,19 +2,24 @@ package com.foxminded.aprihodko.carrestservice.controller;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foxminded.aprihodko.carrestservice.dto.CarDTO;
 import com.foxminded.aprihodko.carrestservice.dto.CarList;
 import com.foxminded.aprihodko.carrestservice.model.Car;
-import com.foxminded.aprihodko.carrestservice.model.CarSearchRequest;
-import com.foxminded.aprihodko.carrestservice.model.search.SearchRequest;
+import com.foxminded.aprihodko.carrestservice.repository.dao.specification.CarSpecification;
 import com.foxminded.aprihodko.carrestservice.service.CarService;
 
 import lombok.RequiredArgsConstructor;
@@ -32,10 +37,24 @@ public class CarRestController {
 //	objectId,Make,Year,Model,Category
 //	ZRgPP9dBMm,Audi,2020,Q3,SUV
 
+//	@GetMapping
+//	Page<CarDTO> findAll(CarSearchRequest request) throws SQLException {
+//		SearchRequest searchRequest = request.asSearchRequest();
+//		return carService.findAllByFilter2(searchRequest).map(CarDTO::fromCar);
+//	}
+
 	@GetMapping
-	Page<CarDTO> findAll(CarSearchRequest request) throws SQLException {
-		SearchRequest searchRequest = request.asSearchRequest();
-		return carService.findAllByFilter2(searchRequest).map(CarDTO::fromCar);
+	ResponseEntity<List<CarDTO>> findAllWithPage(@RequestParam(required = false) Long make_id,
+			@RequestParam(required = false) Long model_id,
+			com.foxminded.aprihodko.carrestservice.model.PageOptions pageOptions) throws SQLException {
+
+		System.out.println(pageOptions);
+		List<Specification<Car>> predicates = Stream
+				.of(Optional.ofNullable(make_id).map(CarSpecification::hasMakeId),
+						Optional.ofNullable(model_id).map(CarSpecification::hasModelId))
+				.filter(Optional::isPresent).map(Optional::get).collect(Collectors.toList());
+
+		return ResponseEntity.ok(CarList.fromCar(carService.findAllByFilter(predicates, pageOptions)));
 	}
 
 	@GetMapping("{id}")
@@ -51,9 +70,9 @@ public class CarRestController {
 
 	}
 
-	@GetMapping("/make{name}")
-	ResponseEntity<CarDTO> findByName(@PathVariable(name = "name") String name) throws SQLException {
-		Car car = carService.findByName(name).get();
-		return ResponseEntity.ok(CarDTO.fromCar(car));
+	@PostMapping()
+	ResponseEntity<CarDTO> findByName(@RequestBody Car car) throws SQLException {
+		Car carToSave = carService.save(car);
+		return ResponseEntity.ok(CarDTO.fromCar(carToSave));
 	}
 }
---
+++ src/test/java/com/foxminded/aprihodko/carrestservice/controller/CarRestControllerTest.java
@@ -0,0 +1,14 @@
+package com.foxminded.aprihodko.carrestservice.controller;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class CarRestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
