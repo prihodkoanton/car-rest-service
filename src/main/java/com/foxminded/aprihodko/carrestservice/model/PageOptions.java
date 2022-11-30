ca131a4e6a31a96d67762de04b3e0f11d4c08cd8
anton.prihodko <anton.prihodko@acronis.com>
2022-11-30 19:35:29 +0400

    Deleted Base test for CarRestController

--- src/main/java/com/foxminded/aprihodko/carrestservice/model/PageOptions.java
+++
@@ -1,9 +0,0 @@
-package com.foxminded.aprihodko.carrestservice.model;
-
-import lombok.Data;
-
-@Data
-public class PageOptions {
-	private Integer page = 0;
-	private Integer pageSize = 10;
-}
--- src/test/resources/sql/make_test_data.sql
+++
@@ -1,7 +0,0 @@
-insert into makes (id, name)
-VALUES (100, 'Audi'),
-       (101, 'BMW'),
-       (102, 'Mercedes'),
-       (103, 'Audi'),
-       (104, 'BMW'),
-       (105, 'Mercedes');
