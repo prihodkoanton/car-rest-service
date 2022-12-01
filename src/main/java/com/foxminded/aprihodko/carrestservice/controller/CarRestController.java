bf171da53600f19b2c95aaeaea63c6b9d4961abf
anton.prihodko <anton.prihodko@acronis.com>
2022-12-01 10:39:27 +0400

    Fixed search for CarRest (IT IS WORK!)

--- src/main/java/com/foxminded/aprihodko/carrestservice/controller/CarRestController.java
+++ src/main/java/com/foxminded/aprihodko/carrestservice/controller/CarRestController.java
@@ -1,10 +1,8 @@
 package com.foxminded.aprihodko.carrestservice.controller;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,15 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.foxminded.aprihodko.carrestservice.dto.CarDTO;
 import com.foxminded.aprihodko.carrestservice.dto.CarList;
 import com.foxminded.aprihodko.carrestservice.model.Car;
-import com.foxminded.aprihodko.carrestservice.model.Category;
-import com.foxminded.aprihodko.carrestservice.model.Make;
-import com.foxminded.aprihodko.carrestservice.model.Model;
 import com.foxminded.aprihodko.carrestservice.model.PageOptions;
 import com.foxminded.aprihodko.carrestservice.repository.dao.specification.CarSpecification;
 import com.foxminded.aprihodko.carrestservice.service.CarService;
-import com.foxminded.aprihodko.carrestservice.service.CategoryService;
-import com.foxminded.aprihodko.carrestservice.service.MakeService;
-import com.foxminded.aprihodko.carrestservice.service.ModelService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -43,9 +35,9 @@ import lombok.RequiredArgsConstructor;
 public class CarRestController {
 
 	private final CarService carService;
-	private final MakeService makeService;
-	private final ModelService modelService;
-	private final CategoryService categoryService;
+//	private final MakeService makeService;
+//	private final ModelService modelService;
+//	private final CategoryService categoryService;
 
 	@GetMapping
 	ResponseEntity<List<CarDTO>> findAllWithPage(@RequestParam(required = false) String make,
@@ -53,8 +45,7 @@ public class CarRestController {
 			@RequestParam(required = false) String category, PageOptions pageOptions) throws SQLException {
 
 		System.out.println(pageOptions);
-		List<Specification<Car>> predicates = new ArrayList<>();
-		predicates = Stream
+		List<Specification<Car>> predicates = Stream
 				.of(Optional.ofNullable(year).map(CarSpecification::hasYear),
 						Optional.ofNullable(category).map(CarSpecification::hasCategoryName),
 						Optional.ofNullable(model).map(CarSpecification::hasModelName),
@@ -70,16 +61,16 @@ public class CarRestController {
 		return ResponseEntity.ok(CarDTO.fromCar(carToSave));
 	}
 
-	@PostMapping("/make/{make_name}/model/{model_name}/category/{category_name}/year/{year}")
-	ResponseEntity<CarDTO> createNew(@PathVariable String make_name, @PathVariable String model_name,
-			@PathVariable String category_name, @PathVariable int year) throws SQLException {
-		Make make = makeService.save(new Make(make_name));
-		Model model = modelService.save(new Model(model_name, make));
-		Category category = categoryService.save(new Category(category_name));
-		Car carToSave = new Car(year, make, model, Set.of(category));
-		Car result = carService.save(carToSave);
-		return ResponseEntity.ok(CarDTO.fromCar(result));
-	}
+//	@PostMapping("/make/{make_name}/model/{model_name}/category/{category_name}/year/{year}")
+//	ResponseEntity<CarDTO> createNew(@PathVariable String make_name, @PathVariable String model_name,
+//			@PathVariable String category_name, @PathVariable int year) throws SQLException {
+//		Make make = makeService.save(new Make(make_name));
+//		Model model = modelService.save(new Model(model_name, make));
+//		Category category = categoryService.save(new Category(category_name));
+//		Car carToSave = new Car(year, make, model, Set.of(category));
+//		Car result = carService.save(carToSave);
+//		return ResponseEntity.ok(CarDTO.fromCar(result));
+//	}
 
 	@DeleteMapping("delete")
 	void deleteByObject(@RequestBody CarDTO dto) throws SQLException {
--- src/main/java/com/foxminded/aprihodko/carrestservice/repository/dao/specification/CarSpecification.java
+++ src/main/java/com/foxminded/aprihodko/carrestservice/repository/dao/specification/CarSpecification.java
@@ -1,13 +1,14 @@
 package com.foxminded.aprihodko.carrestservice.repository.dao.specification;
 
-import javax.persistence.criteria.Join;
+import java.util.Collection;
+
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
 
 import org.springframework.data.jpa.domain.Specification;
 
 import com.foxminded.aprihodko.carrestservice.model.Car;
 import com.foxminded.aprihodko.carrestservice.model.Category;
-import com.foxminded.aprihodko.carrestservice.model.Make;
-import com.foxminded.aprihodko.carrestservice.model.Model;
 
 public class CarSpecification {
 
@@ -16,23 +17,20 @@ public class CarSpecification {
 	}
 
 	public static Specification<Car> hasMakeName(String makeName) {
-		return (root, cq, cb) -> {
-			Join<Make, Car> carMake = root.join("make");
-			return cb.equal(cb.lower(carMake.<String>get("name")), makeName);
-		};
+		return (root, cq, cb) -> cb.equal(root.get("make").get("name"), makeName);
 	}
 
 	public static Specification<Car> hasModelName(String modelName) {
-		return (root, cq, cb) -> {
-			Join<Model, Car> carModel = root.join("model");
-			return cb.equal(cb.lower(carModel.<String>get("name")), modelName);
-		};
+		return (root, cq, cb) -> cb.equal(root.get("model").get("name"), modelName);
 	}
 
 	public static Specification<Car> hasCategoryName(String categoryName) {
 		return (root, cq, cb) -> {
-			Join<Category, Car> carCategory = root.join("categories");
-			return cb.equal(carCategory.get("name"), categoryName);
+			cq.distinct(true);
+			Root<Car> car = root;
+			Root<Category> category = cq.from(Category.class);
+			Expression<Collection<Car>> carCategory = car.get("categories");
+			return cb.and(cb.equal(category.get("name"), categoryName), cb.isMember(car, carCategory));
 		};
 	}
 }
